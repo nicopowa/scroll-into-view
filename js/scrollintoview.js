1a313107545b435cc5c4760d09e298d7ee816966
nicopowa <nicopowa@gmail.com>
2019-02-14 11:35:17 +0100

    update body bug if already scrolled

--- js/scrollintoview.js
+++ js/scrollintoview.js
@@ -1,10 +1,10 @@
 function scrollIntoView(el) {
 	for(
 		var p = el.parentNode, e = el.getBoundingClientRect(), t = p.getBoundingClientRect(), s = window.getComputedStyle(p)["overflow-y"]; 
-		p != document.body && (s === "visible" || Math.round(t.height) >= p.scrollHeight); // wtf firefox *.5 rect height
+		p != document.body && (s === "visible" || Math.round(t.height) >= p.scrollHeight);
 		p = p.parentNode, t = p.getBoundingClientRect(), s = window.getComputedStyle(p)["overflow-y"]
 	) {}
-	if(p === document.body) document.documentElement.scrollTop = p.scrollTop = p.scrollTop + e.top - t.top; // body.scrollTop deprecated
+	if(p === document.body) document.documentElement.scrollTop = p.scrollTop = e.top - t.top;
 	else p.scrollTop = p.scrollTop + e.top - t.top;
 }
 
